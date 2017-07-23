57093693bd67a4991eed6c1e9255db8977c3f737
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-23 20:55:59 +0200

    Fundet ud af landskabet, rigidbody, bevægelse + usynynlige rammer

--- Assets/Scripts/Skildmoves.cs
+++ Assets/Scripts/Skildmoves.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Skildmoves : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    public float moveSpeed = 20;
+    public Rigidbody myRigidbody;
+    public float rotationSpeed = 60;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+        transform.Rotate(0, rotationSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0);
 	}
+
+    void FixedUpdate() {
+        Move(moveSpeed * Input.GetAxis("Vertical"));
+    }
+
+    void Move(float speed) {
+        myRigidbody.velocity = transform.forward * speed + Vector3.up * myRigidbody.velocity.y;
+    }
 }
