3e987bcb90110b019a5eaf43212624a4e601ffd3
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-23 21:49:46 +0200

    Krapmoves

---
+++ Assets/Scripts/Krappemoves.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Krappemoves : MonoBehaviour {
+
+    public Rigidbody KrapsRigidbody;
+    public float moveSpeed = 20;
+    public float rotationSpeed = 60;
+    Transform player;
+   
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag("Skildpadde").transform;
+
+     
+
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
