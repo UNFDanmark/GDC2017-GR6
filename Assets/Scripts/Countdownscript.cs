312e30bc232a5aa5ed40ca417667252bfea3e805
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-24 19:42:52 +0200

    Lose and Win-scene

---
+++ Assets/Scripts/Countdownscript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Countdownscript : MonoBehaviour {
+    
+    public int starttid = 60;
+    public float countdown = 1;
+    public float sluttid = 0;
+    public Text countdownText;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Nedtælling();
+	}
+        public void tid = (starttid - countdown) * Time.deltatime;
+        public void Nedtælling()
+        {
+        if (tid = sluttid) SceneManager.LoadScene("Winscene");
+    }
+        
+}
