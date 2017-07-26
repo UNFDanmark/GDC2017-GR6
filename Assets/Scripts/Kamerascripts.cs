d01a0d80f099c3fe6b6adc0413a2ede363916bb5
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-26 22:29:34 +0200

    Win og loosescene

---
+++ Assets/Scripts/Kamerascripts.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Kamerascripts : MonoBehaviour {
+
+    public GameObject Skildpadde;
+    public Vector3 PindTilSkildpadde; 
+
+	// Use this for initialization
+	void Start () {
+
+        PindTilSkildpadde = transform.position - Skildpadde.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = PindTilSkildpadde + Skildpadde.transform.position;
+    	
+	}
+}
